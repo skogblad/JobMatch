dc95a134da66c6ef7e8a6132fcee985cb3d1c2e9
Karin Henriksson <karin.henriksson@medieinstitutet.se>
2025-09-10 13:12:02 +0200

    ✨ feat: add getJobById function

--- src/services/JobService.ts
+++ src/services/JobService.ts
@@ -1,4 +1,4 @@
-import type { IJobSearchRes } from "../models/IJob";
+import { type IJob, type IJobSearchRes } from "../models/IJob";
 import { get } from "./serviceBase";
 
 const BASE_URL = "https://jobsearch.api.jobtechdev.se";
@@ -8,4 +8,10 @@ export const getJobs = async (limit: number = 10) => {
 
     const data = await get<IJobSearchRes>(url);
     return data.hits;
+}
+
+export const getJobById = async (id: string) => {
+    const url = `${BASE_URL}/ad/${id}`;
+    return await get<IJob>(url);
+
 }
