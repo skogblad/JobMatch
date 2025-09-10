4b5e067d9bdeb52286b81b1397b1bbb9adbc25f1
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-09-10 17:09:29 +0200

    ♻️ refactor(pages): update job list page

--- src/pages/ListPage/JobList.tsx
+++ src/pages/ListPage/JobList.tsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from "react";
-import { Link, useSearchParams } from "react-router";
-import { getJobs } from "../../services/JobService";
-import type { IJob } from "../../models/IJob";
-import {
-  DigiLayoutContainer,
-  DigiList,
-  DigiLayoutMediaObject,
-  DigiMediaImage,
-  DigiTypography,
-  DigiLayoutColumns,
-} from "@digi/arbetsformedlingen-react";
 import "./JobList.css";
 import placeholder from "../../assets/placeholder.svg";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router";
+import { getJobs } from "../../services/JobService";
+import type { IJob } from "../../models/IJob";
+
 import {
-  LayoutColumnsElement,
-  LayoutColumnsVariation,
+  DigiLayoutContainer,
+  DigiLayoutMediaObject,
+  DigiLink,
+  DigiList,
+  DigiMediaImage,
+  DigiTypography,
+} from "@digi/arbetsformedlingen-react";
+
+import {
   LayoutMediaObjectAlignment,
   TypographyVariation,
 } from "@digi/arbetsformedlingen";
@@ -34,39 +34,32 @@ export const JobList = () => {
   }, [searchText]);
 
   return (
-    <DigiLayoutContainer>
-      <DigiLayoutColumns
-        className="page-column"
-        afElement={LayoutColumnsElement.DIV}
-        afVariation={LayoutColumnsVariation.ONE}
-      >
-        <DigiList className="job-list">
-          {jobs.map((j) => (
-            <li className="job-list-item" key={j.id}>
-              <DigiLayoutMediaObject
-                afAlignment={LayoutMediaObjectAlignment.CENTER}
-              >
-                <DigiMediaImage className="item-img" slot="media">
-                  {j.logo_url ? (
-                    <img src={j.logo_url} alt={j.headline} />
-                  ) : (
-                    <img src={placeholder} alt="Placeholder image" />
-                  )}
-                </DigiMediaImage>
-
-                <DigiTypography afVariation={TypographyVariation.SMALL}>
-                  <Link to={`/jobs/${j.id}`}>
-                    <h3>{j.headline}</h3>
-                  </Link>
-
-                  <p>{j.occupation.label}</p>
-                  <p>Publiserad {j.publication_date}</p>
-                </DigiTypography>
-              </DigiLayoutMediaObject>
-            </li>
-          ))}
-        </DigiList>
-      </DigiLayoutColumns>
+    <DigiLayoutContainer className="page-container">
+      <DigiList className="job-list">
+        {jobs.map((j) => (
+          <li key={j.id}>
+            <DigiLayoutMediaObject
+              className="job-list-item"
+              afAlignment={LayoutMediaObjectAlignment.CENTER}
+            >
+              <DigiMediaImage slot="media" className="item-img">
+                {j.logo_url ? (
+                  <img src={j.logo_url} alt={j.headline} />
+                ) : (
+                  <img src={placeholder} alt="Placeholder image" />
+                )}
+              </DigiMediaImage>
+              <DigiTypography afVariation={TypographyVariation.SMALL}>
+                <DigiLink afHref={`/jobs/${j.id}`}>
+                  <h3>{j.headline}</h3>
+                </DigiLink>
+                <p>{j.occupation.label}</p>
+                <p>Publiserad {j.publication_date}</p>
+              </DigiTypography>
+            </DigiLayoutMediaObject>
+          </li>
+        ))}
+      </DigiList>
     </DigiLayoutContainer>
   );
 };
