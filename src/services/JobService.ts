6d621668a4299d89f30fa8b337b763d595ecbbf5
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-09-12 13:25:11 +0200

    ✨ feat(pages): create pagination

--- src/services/JobService.ts
+++ src/services/JobService.ts
@@ -3,19 +3,24 @@ import { get } from "./serviceBase";
 
 const BASE_URL = "https://jobsearch.api.jobtechdev.se";
 
-export const getJobs = async (searchText?: string, limit: number = 10) => {
-    let url = `${BASE_URL}/search?limit=${limit}`;
+export const getJobs = async (
+  searchText?: string,
+  limit: number = 10,
+  page: number = 1
+) => {
+  const offset = (page - 1) * limit;
 
-    if (searchText && searchText.trim() !== "") {
-        url += `&q=${encodeURIComponent(searchText)}`;
-    }
+  let url = `${BASE_URL}/search?limit=${limit}&offset=${offset}`;
 
-    const data = await get<IJobSearchRes>(url);
-    return data.hits;
-}
+  if (searchText && searchText.trim() !== "") {
+    url += `&q=${encodeURIComponent(searchText)}`;
+  }
+
+  const data = await get<IJobSearchRes>(url);
+  return data;
+};
 
 export const getJobById = async (id: string) => {
-    const url = `${BASE_URL}/ad/${id}`;
-    return await get<IJob>(url);
-
-}
+  const url = `${BASE_URL}/ad/${id}`;
+  return await get<IJob>(url);
+};
